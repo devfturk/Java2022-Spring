7aabd7dda99509c5186245099cf664ee0cdbac58
devfturk <furkanturk0@outlook.com.tr>
2023-03-27 03:16:35 +0300

    First commit Spring giriş 4. gün ders sonu

---
+++ src/main/java/kodlama/io/rentAcar/business/abstracts/BrandService.java
@@ -0,0 +1,9 @@
+package kodlama.io.rentAcar.business.abstracts;
+
+import java.util.List;
+
+import kodlama.io.rentAcar.entities.concretes.Brand;
+
+public interface BrandService {
+	List<Brand> getAll();
+}
---
+++ src/main/java/kodlama/io/rentAcar/dataAccess/concretes/InMemoryBrandRepository.java
@@ -0,0 +1,35 @@
+package kodlama.io.rentAcar.dataAccess.concretes;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.stereotype.Repository;
+
+import kodlama.io.rentAcar.dataAccess.abstracts.BrandRepository;
+import kodlama.io.rentAcar.entities.concretes.Brand;
+
+@Repository // bu sınıf bir dataAccess nesnesidir.
+public class InMemoryBrandRepository implements BrandRepository {
+
+	List<Brand> brands;
+	
+
+	public InMemoryBrandRepository() {
+		
+		brands = new ArrayList<Brand>();
+		brands.add(new Brand(1,"BMW"));
+		brands.add(new Brand(2,"Mercedes"));
+		brands.add(new Brand(3,"Audi"));
+		brands.add(new Brand(4,"Fiat"));
+		brands.add(new Brand(5,"Renault"));
+	}
+
+
+
+	@Override
+	public List<Brand> getAll() {
+		
+		return brands;
+	}
+
+}
